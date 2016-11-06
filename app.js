bb31be49049bca2b10acc81e2fe57fc421913217
Victor Villacis <victor.villacis@outlook.com>
2016-11-06 15:25:05 -0500

    add angular routes and views, add, edit, view ,delete

--- app.js
+++ app.js
@@ -1,77 +1,77 @@
-//dependencies 
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser')
-var mongoose    = require('mongoose');
+//dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
 
 //specify what folder I want to use as a static folder, public directory
-app.use(express.static(__dirname+'client'))
+app.use(express.static(__dirname + '/client'));
 //initialize body parser
 app.use(bodyParser.json());
 
 //to access the properties Using the Genre and Book Object
 Genre = require('./models/genre.js')
 Book = require('./models/book.js')
-//connect to mongoose
+    //connect to mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 //database object
-var db = mongoose.connection
+var db = mongoose.connection;
 //routes
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('Please navigate to - /api/books or /api/genres');
 });
 
-app.get('/api/genres', function(req,res){
-    Genre.getGenres(function(err, genres){
-      if(err){
-        throw err;
-      }
-      res.json(genres);
+app.get('/api/genres', function(req, res) {
+    Genre.getGenres(function(err, genres) {
+        if (err) {
+            throw err;
+        }
+        res.json(genres);
     });
 });
 
-app.get('/api/genres/:_id', function(req,res){
-    Genre.getGenreById(req.params._id, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
+app.get('/api/genres/:_id', function(req, res) {
+    Genre.getGenreById(req.params._id, function(err, genre) {
+        if (err) {
+            throw err;
+        }
+        res.json(genre);
     });
 });
 
 
-app.post('/api/genres', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.post('/api/genres', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var genre = req.body;
-    Genre.addGenre(genre, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
+    Genre.addGenre(genre, function(err, genre) {
+        if (err) {
+            throw err;
+        }
+        res.json(genre);
     });
 });
 
-app.put('/api/genres/:_id', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.put('/api/genres/:_id', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var id = req.params._id;
     var genre = req.body;
-    Genre.updateGenre(id, genre, {}, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
+    Genre.updateGenre(id, genre, {}, function(err, genre) {
+        if (err) {
+            throw err;
+        }
+        res.json(genre);
     });
 });
 
-app.delete('/api/genres/:_id', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.delete('/api/genres/:_id', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var id = req.params._id;
     //this is just gonna take in an id and a call back
-    Genre.removeGenre(id, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
+    Genre.removeGenre(id, function(err, genre) {
+        if (err) {
+            throw err;
+        }
+        res.json(genre);
     });
 });
 
@@ -90,57 +90,57 @@ app.delete('/api/genres/:_id', function(req,res){
 //     });
 // });
 
-app.get('/api/books', function(req,res){
-    Book.getBooks(function(err, books){
-      if(err){
-        throw err;
-      }
-      res.json(books);
+app.get('/api/books', function(req, res) {
+        Book.getBooks(function(err, books) {
+            if (err) {
+                throw err;
+            }
+            res.json(books);
+        })
     })
-})
-//id will represent the id, gets only book by the id
-app.get('/api/books/:_id', function(req,res){
-  //take in the id passed into the URL
-    Book.getBookById(req.params._id, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
+    //id will represent the id, gets only book by the id
+app.get('/api/books/:_id', function(req, res) {
+    //take in the id passed into the URL
+    Book.getBookById(req.params._id, function(err, book) {
+        if (err) {
+            throw err;
+        }
+        res.json(book);
     })
 })
 
-app.post('/api/books', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.post('/api/books', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var book = req.body;
-    Book.addBook(book, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
+    Book.addBook(book, function(err, book) {
+        if (err) {
+            throw err;
+        }
+        res.json(book);
     });
 });
 
-app.put('/api/books/:_id', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.put('/api/books/:_id', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var id = req.params._id;
     var book = req.body;
-    Book.updateBook(id, book, {}, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
+    Book.updateBook(id, book, {}, function(err, book) {
+        if (err) {
+            throw err;
+        }
+        res.json(book);
     });
 });
 
-app.delete('/api/books/:_id', function(req,res){
-  //body parser allows us to acces anything that comes into the form
+app.delete('/api/books/:_id', function(req, res) {
+    //body parser allows us to acces anything that comes into the form
     var id = req.params._id;
     //this is just gonna take in an id and a call back
-    Book.removeBook(id, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
+    Book.removeBook(id, function(err, book) {
+        if (err) {
+            throw err;
+        }
+        res.json(book);
     });
 });
 
---
+++ client/controllers/books.js
@@ -0,0 +1,33 @@
+var myApp = angular.module('myApp');
+myApp.controller('BooksController', ['$scope', '$http', '$location',
+    '$routeParams',
+    function($scope, $http, $location, $routeParams) {
+        console.log('BooksController loaded...')
+        $scope.getBooks = function() {
+            $http.get('/api/books').success(function(response) {
+                $scope.books = response
+            });
+        }
+
+        $scope.getBook = function() {
+            var id = $routeParams.id;;
+            $http.get('/api/books/' + id).success(function(
+                response) {
+                $scope.book = response;
+            });
+        }
+
+        $scope.addBook = function() {
+            //what do we want to post? $scope.book
+            $http.post('/api/books/', $scope.book).success(function(
+                response) {
+                //redirect
+                window.location.href = '#/books';
+            });
+        }
+    }
+]);
+
+
+//do not use the minify version of angular,so to avoid of it breaking in minified you need to specify it here
+//scope binds the controller to the view, http aloows us to get request and post, route params allows us to get variable
